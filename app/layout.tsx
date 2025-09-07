4aebec5431681963545c95bf39780a88a13a842d
BerzanIrma <irmabia2005@gmail.com>
2025-09-07 14:34:13 +0300

    Form components

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,9 @@
-import './globals.css'
 import { Inter } from 'next/font/google'
 import { ClerkProvider } from '@clerk/nextjs'
-import { StoreModal } from '@/components/ui/store-modal'
+
+import { ModalProvider } from '@/providers/modal-provider'
+
+import './globals.css'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -19,8 +21,8 @@ export default function RootLayout({
     <ClerkProvider>
       <html lang="en">
          <body className={inter.className}>
-           <StoreModal />
-           {children}
+          <ModalProvider />
+          {children}
          </body>
       </html>   
     </ClerkProvider>
